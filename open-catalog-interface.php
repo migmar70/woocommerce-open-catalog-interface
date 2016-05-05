f72c1ca83c13b88b358f9d0be15bcde3d9fb9f70
Miguel Martinez <mmartinez4@lifesize.com>
2016-05-05 00:03:10 -0500

    Updated contact information and license

--- open-catalog-interface.php
+++ open-catalog-interface.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * Plugin Name: PunchOut Open Catalog Interface for WooCommerce
- * Plugin URI: http://www.miguelmartinez.com/
+ * Plugin URI: 	http://www.miguelmartinez.com/
  * Description: Simple plugin implementation of a PunchOut Catalog using Open Catalog Interface for WooCommerce.
- * Version: 0.0.1
- * Author: Miguel Martinez <punchout.oci@miguelmartinez.com>
- * Author URI: http://miguelmartinez.com
+ * Version: 	0.0.1
+ * Author: 		Miguel Martinez <punchout.oci@miguelmartinez.com>
+ * Author URI: 	http://miguelmartinez.com
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
